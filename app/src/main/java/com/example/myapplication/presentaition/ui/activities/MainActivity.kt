2d5aac0389b3a061d50da4f3720d71521cb32c67
studentColl <ggxaoc@gmail.com>
2025-03-11 09:49:51 +0300

    a

--- app/src/main/java/com/example/myapplication/presentaition/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentaition/ui/activities/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.myapplication.presentaition.ui.activities
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.R
 import com.example.myapplication.data.application.MyApplication
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.domain.models.Note
 import com.example.myapplication.domain.usecases.GetNotesUseCase
-import com.example.myapplication.presentaition.ui.fragments.registration.RegistrationFragment
-import com.example.myapplication.presentaition.ui.fragments.fragmentfactory.MFragmentFactory
+//import com.example.myapplication.presentaition.ui.fragments.registration.RegistrationFragment
+//import com.example.myapplication.presentaition.ui.fragments.fragmentfactory.MFragmentFactory
 import com.example.myapplication.presentaition.viewmodelfactories.NoteViewModelFactory
 import com.example.myapplication.presentaition.viewmodels.AddNoteViewModel
 import com.example.myapplication.presentaition.viewmodels.NoteViewModel
@@ -19,49 +21,49 @@ class MainActivity : AppCompatActivity() {
     private lateinit var addNoteViewModel: AddNoteViewModel
     private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        supportFragmentManager.fragmentFactory = MFragmentFactory("first fragment", 1)
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        if(savedInstanceState == null){
-            replaceFragment(RegistrationFragment::class.java.toString())
-        }
-
-        // Get the Application context properly
-        val app = applicationContext as MyApplication
-        val userRepository = app.getNoteRepositoryImpl
-        //val addUserUseCase = AddUserUseCase(userRepository)
-        val getNotesUseCase = GetNotesUseCase(userRepository)
-        val viewModelFactory = NoteViewModelFactory(getNotesUseCase)
-        //val addUserViewModelFactory = AddUserViewModelFactory(addUserUseCase)
-
-
-
-
-        noteViewModel = ViewModelProvider(this, viewModelFactory)[NoteViewModel::class.java]
-        //addUserViewModel = ViewModelProvider(this, addUserViewModelFactory)[AddUserViewModel::class.java]
-        /*
-        binding.buttonId.setOnClickListener{
-            val name = binding.editNameId.text.toString()
-            val surname = binding.editSurnameId.text.toString()
-            val age = binding.editAgeId.text.toString()
-
-            if(name.isNotEmpty() && surname.isNotEmpty() && age.isNotEmpty()){
-
-                addUserViewModel.addUser(User(name = name, surname = surname, age= age.toInt()))
-            }
-            lifecycleScope.launchWhenResumed {
-
-                userViewModel.users.collect() { users ->
-                    displayUsers(users)
-                }
-            }
-        }
-        */
-
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        supportFragmentManager.fragmentFactory = MFragmentFactory("first fragment", 1)
+//        super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        if(savedInstanceState == null){
+//            replaceFragment(RegistrationFragment::class.java.toString())
+//        }
+//
+//        // Get the Application context properly
+//        val app = applicationContext as MyApplication
+//        val userRepository = app.getNoteRepositoryImpl
+//        //val addUserUseCase = AddUserUseCase(userRepository)
+//        val getNotesUseCase = GetNotesUseCase(userRepository)
+//        val viewModelFactory = NoteViewModelFactory(getNotesUseCase)
+//        //val addUserViewModelFactory = AddUserViewModelFactory(addUserUseCase)
+//
+//
+//
+//
+//        noteViewModel = ViewModelProvider(this, viewModelFactory)[NoteViewModel::class.java]
+//        //addUserViewModel = ViewModelProvider(this, addUserViewModelFactory)[AddUserViewModel::class.java]
+//        /*
+//        binding.buttonId.setOnClickListener{
+//            val name = binding.editNameId.text.toString()
+//            val surname = binding.editSurnameId.text.toString()
+//            val age = binding.editAgeId.text.toString()
+//
+//            if(name.isNotEmpty() && surname.isNotEmpty() && age.isNotEmpty()){
+//
+//                addUserViewModel.addUser(User(name = name, surname = surname, age= age.toInt()))
+//            }
+//            lifecycleScope.launchWhenResumed {
+//
+//                userViewModel.users.collect() { users ->
+//                    displayUsers(users)
+//                }
+//            }
+//        }
+//        */
+//
+//    }
 
     private fun addFragment(fragmentName: String,) {
         val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, fragmentName)
@@ -71,7 +73,7 @@ class MainActivity : AppCompatActivity() {
             .commitAllowingStateLoss()
     }
 
-    fun replaceFragment(fragmentName: String,) {
+    private fun replaceFragment(fragmentName: String,) {
         val fragment = supportFragmentManager.fragmentFactory.instantiate(classLoader, fragmentName)
         supportFragmentManager
             .beginTransaction()
@@ -80,15 +82,15 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun displayNotes(Notes: List<Note>){
-        /*
+    private fun displayNotes(notes: List<Note>){
+
         val sb = StringBuilder()
-        users.forEach{
-            user ->
-            sb.append("${user.name}\n")
-            Log.d("SecondTag",  user.toString())
+        notes.forEach{
+            note ->
+            sb.append("${note.title}\n")
+            Log.d("SecondTag",  note.toString())
         }
-        binding.surnameId.text = sb.toString()
-        */
+        //binding.surnameId.text = sb.toString()
+
     }
 }
